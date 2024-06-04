59e2cd2fca8ec274f07ebe1129b5c698ae36541a
Documenter.jl <documenter@juliadocs.github.io>
2024-06-04 09:02:55 +0000

    build based on 74d2786

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LuxorTensorPlot","category":"page"},{"location":"#LuxorTensorPlot","page":"Home","title":"LuxorTensorPlot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LuxorTensorPlot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LuxorTensorPlot]","category":"page"}]
+}
